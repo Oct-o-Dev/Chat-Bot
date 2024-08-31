91dcc3e9ca6287878c75af5484aa0e6a9f5b9274
Oct-o-Dev <Abhineetk213@gmail.com>
2024-08-31 15:31:19 +0530

    prod ready

--- server/index.js
+++ server/index.js
@@ -14,6 +14,26 @@ app.use(express.json());
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoute);
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1,"..", "public/chat-app/build")));
+
+  app.get("*", (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname1, "..", "public/chat-app", "build", "index.html")
+		)
+	);
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
